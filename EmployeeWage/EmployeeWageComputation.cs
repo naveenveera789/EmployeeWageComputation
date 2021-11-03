760fc6a236c217e090ad312fdc393a02e73b9104
naveenveera789 <naveenveera789@gmail.com>
2021-11-03 19:29:06 +0530

    [naveenveera789] add : Daily wage using switch case

--- EmployeeWage/EmployeeWageComputation.cs
+++ EmployeeWage/EmployeeWageComputation.cs
@@ -7,20 +7,22 @@ namespace EmployeeWage
     public class EmployeeWageComputation
     {
         public const int IS_FULL_TIME = 1, IS_PART_TIME = 2, WAGE_PER_HR = 20, FULL_WORKING_HRS = 8, PART_WORKING_HRS = 4;
-        int totalEmpWage,empHrs = 0;
+        int totalEmpWage,empHrs;
         public void DailyEmployeeWage()
         {
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                this.empHrs = FULL_WORKING_HRS;
-                Console.WriteLine("Employee worked Full time");
-            }
-            if (empCheck == IS_PART_TIME)
-            {
-                 this.empHrs = PART_WORKING_HRS;
-                Console.WriteLine("Employee worked Part time");
+               case IS_FULL_TIME: this.empHrs = FULL_WORKING_HRS;
+                    Console.WriteLine("Employee worked Full time");
+                    break;
+               case IS_PART_TIME: this.empHrs = PART_WORKING_HRS;
+                    Console.WriteLine("Employee worked Half time");
+                    break;
+               default: this.empHrs = 0;
+                    Console.WriteLine("Employee is absent");
+                    break;
             }
             Console.WriteLine(this.totalEmpWage = this.empHrs * WAGE_PER_HR);
         }
