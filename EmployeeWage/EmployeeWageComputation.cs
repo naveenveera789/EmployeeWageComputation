9e48aebea44298988ef458a00bfe5feb734fa13b
naveenveera789 <naveenveera789@gmail.com>
2021-11-03 19:03:52 +0530

    [naveenveera789] add : Daily Working based Employee Wage

--- EmployeeWage/EmployeeWageComputation.cs
+++ EmployeeWage/EmployeeWageComputation.cs
@@ -6,20 +6,23 @@ namespace EmployeeWage
 {
     public class EmployeeWageComputation
     {
-        public const int IS_PRESENT = 1, WAGE_PER_HR = 20, FULL_WORKING_HRS = 8;
-        int totalEmpWage;
+        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2, WAGE_PER_HR = 20, FULL_WORKING_HRS = 8, PART_WORKING_HRS = 4;
+        int totalEmpWage,empHrs = 0;
         public void DailyEmployeeWage()
         {
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_PRESENT)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_FULL_TIME)
             {
-                this.totalEmpWage = FULL_WORKING_HRS * WAGE_PER_HR;
-                Console.WriteLine("Employee is present");
-                Console.WriteLine(this.totalEmpWage);
+                this.empHrs = FULL_WORKING_HRS;
+                Console.WriteLine("Employee worked Full time");
             }
-            else
-                Console.WriteLine("Employee is absent");
+            if (empCheck == IS_PART_TIME)
+            {
+                 this.empHrs = PART_WORKING_HRS;
+                Console.WriteLine("Employee worked Part time");
+            }
+            Console.WriteLine(this.totalEmpWage = this.empHrs * WAGE_PER_HR);
         }
     }
 }
